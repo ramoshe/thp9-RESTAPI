dcfb51f1a31d2a83d389f5a74758774250f13730
ramoshe <rsherard@mac.com>
2021-08-15 21:36:30 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -15,10 +15,7 @@ const router = express.Router();
 // Route that returns all properties and values for currently authenticated user.
 router.get('/users', authenticateUser, asyncHandler(async(req, res) => {
     const user = req.currentUser;
-    res.json({
-        name: `${user.firstName} ${user.lastName}`,
-        email: user.emailAddress
-    });
+    res.json({ user });
 }));
 
 // Route that creates a new user.
