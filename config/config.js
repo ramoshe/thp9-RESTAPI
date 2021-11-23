f0189cc96c29c02ac578c8094a75ce3c03b44f14
Rachel Sherard <rsherard@mac.com>
2021-11-22 23:13:06 -0500

    update for Postgres database connection

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_URI } = process.env;
+const { DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_URI } = process.env;
 
 module.exports = {
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
+    "username": 'postgres',
+    "password": 'postgres',
     "database": "courseapi_database_dev",
-    "host": DB_HOST,
+    "host": '127.0.0.1',
     "dialect": "postgres"
   },
   "test": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
+    "username": DB_USER,
+    "password": DB_PASS,
     "database": "courseapi_database_test",
     "host": DB_HOST,
     "dialect": "postgres"
   },
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": "d7o1kg98ers0fu",
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": DB_NAME,
     "host": DB_HOST,
     "uri": DB_URI,
     "dialect": "postgres",
